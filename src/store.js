de4483758a450c6363ca858f06ee4428ec754f47
Alexandr Varabyou <vorobev.profi@gmail.com>
2017-11-22 12:59:01 +0300

    TASK-1: fixed REDUX dev tools connection

--- src/store.js
+++ src/store.js
@@ -1,16 +1,19 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { composeWithDevTools } from 'remote-redux-devtools';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 
 import initialReducer from './reducers/initialReducer';
 import initialReducerMiddleware from './middlewares/initialReducerMiddleware';
 
-const composeEnhancers = composeWithDevTools({ realtime: true });
+const DEVTOOLS_COMPOSER = '__REDUX_DEVTOOLS_EXTENSION_COMPOSE__';
+const composeWithDevTools = (typeof window === 'object' &&
+                            typeof window[DEVTOOLS_COMPOSER] === 'function')
+  ? window[DEVTOOLS_COMPOSER]
+  : compose;
 
 const store = createStore(
   combineReducers({
     init: initialReducer,
   }),
-  composeEnhancers(applyMiddleware(initialReducerMiddleware)),
+  composeWithDevTools(applyMiddleware(initialReducerMiddleware)),
 );
 
 export default store;
