16eb796b899bc7de854d9e95f92273ddf46591f1
Alexandr Varabyou <vorobev.profi@gmail.com>
2017-11-24 00:03:55 +0300

    TASK-2: updated eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,37 @@
 module.exports = {
-    root: true,
-    parser: 'babel-eslint',
-    "extends": 'airbnb',
-    "settings": {
+  root: true,
+  parser: 'babel-eslint',
+  "extends": 'airbnb',
+  "settings": {
     "import/resolver": {
       "babel-module": {
         "extensions": [".js"]
       }
     },
     "import/extensions": [".js"]
-    },
-    "plugins": [
-      "react",
-    ],
-    "env": {
+  },
+  "plugins": [
+    "react",
+  ],
+  "env": {
     "es6": true,
     "node": true
-    },
-    "parserOptions": {
-      "ecmaFeatures": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "jsx": true
-      }
-    },
-    'rules': {
-      "indent": ["error", 2],
-      "react/jsx-indent": [1, 'tab'|2],
-      "react/prefer-stateless-function": [0, { "ignorePureComponents": true }],
-      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-      "react/forbid-prop-types": [0, { "forbid": false }],
-      "no-underscore-dangle": ["error", { "allow": ["_id"] }],
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "ecmaVersion": 6,
+      "sourceType": "module",
+      "jsx": true
     }
+  },
+  'rules': {
+    "indent": ["error", 2],
+    "react/jsx-indent": [1, 'tab'|2],
+    "react/prefer-stateless-function": [0, { "ignorePureComponents": true }],
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/forbid-prop-types": [0, { "forbid": false }],
+    "no-underscore-dangle": ["error", { "allowAfterThis": true }],
+    "eslint no-use-before-define": ["error", { "variables": false }],
+    "eslint import/no-unresolved": [2, { ignore: ['*'] }],
+  }
 }
