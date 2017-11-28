092107553616ccfc3318f3cadc998aa664343888
Aliaksandr Varabyou <aliaksandr_varabyou@epam.com>
2017-11-28 17:34:23 +0300

    TASK-2: added TEMP auth data

--- middlewares/initialReducerMiddleware.js
+++ middlewares/initialReducerMiddleware.js
@@ -2,9 +2,16 @@ export default function initReducerMiddleware({ dispatch }) {
   return next =>
     (action) => {
       if (action.type === 'AUTH_REQUEST') {
+        // Temp mock
+        const authData = {
+          isAuthorised: true,
+          username: 'NativeWriter',
+          position: 'Junior Software Developer',
+        };
+
         return dispatch({
           type: 'AUTH_RESPONSE',
-          payload: { isAuthorised: true },
+          payload: authData,
         });
       }
       return next(action);
