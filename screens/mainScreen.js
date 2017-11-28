8df380d7da5cf20289dd43e7815cac974367f980
Aliaksandr Varabyou <aliaksandr_varabyou@epam.com>
2017-11-28 17:38:07 +0300

    TASK-2: added modal window to MainScreen

--- screens/mainScreen.js
+++ screens/mainScreen.js
@@ -1,42 +1,77 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Modal, Text, StyleSheet, Button } from 'react-native';
 import { NavigationActions } from 'react-navigation';
+import { connect } from 'react-redux';
+import { Ionicons } from '@expo/vector-icons';
+import mapActionsToProps from '../config/actions';
+import PropTypes from 'prop-types';
 
-const testNavAction = NavigationActions.navigate({
-  routeName: 'Actions',
-  params: {},
-  action: NavigationActions.navigate({ routeName: 'SubProfileRoute' }),
-});
-
-export default class MainScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => ({
+class MainScreen extends React.Component {
+  static navigationOptions = ({ screenProps }) => ({
     title: 'Main screen',
     headerRight:
-  <Button
+  <Ionicons
+    style={{ marginRight: 20 }}
+    name="md-person"
+    size={32}
     color="black"
-    title="Button"
-    onPress={() => {
-      navigation.dispatch(testNavAction);
-    }}
+    onPress={screenProps.mainScreen.openMainScreenModal}
   />,
   });
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthorised: false,
+    };
+
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  closeModal() {
+    this.props.closeMainScreenModal();
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>MainScreen</Text>
-        <Button
-          onPress={() => this.props.navigation.navigate('Actions')}
-          title="Go to Actions"
-        />
-        <Button
-          onPress={() => this.props.navigation.navigate('About')}
-          title="Go to About"
-        />
+        <Modal
+          animationType="slide"
+          transparent
+          visible={this.props.mainScreen.isOpenedModal}
+          onRequestClose={() => { alert('Modal has been closed.'); }}
+        >
+          <View style={styles.modalContent}>
+            <Ionicons
+              style={styles.modalCloseButton}
+              name="ios-close-circle-outline"
+              size={32}
+              color="white"
+              onPress={this.closeModal}
+            />
+            <Text style={styles.modalHeader}>{this.props.user.username}</Text>
+            <Text style={styles.modalHeader}>{this.props.user.position}</Text>
+            <View style={styles.modalItemsContainer}>
+              <Button
+                onPress={() => {
+                  this.props.navigation.navigate('About');
+                  this.closeModal();
+                }}
+                title="Go to About"
+              />
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
 }
+
+MainScreen.propTypes = {
+  mainScreen: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -45,4 +80,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  modalContent: {
+    flex: 1,
+    paddingTop: 22,
+    backgroundColor: 'rgba(52, 52, 52, 0.9)',
+  },
+  modalHeader: {
+    color: 'white',
+    alignSelf: 'center',
+    marginTop: 40,
+  },
+  modalCloseButton: {
+    alignSelf: 'flex-end',
+    marginRight: 20,
+  },
+  modalItemsContainer: {
+    marginRight: 50,
+  },
 });
+
+function mapStateToProps(state) {
+  return {
+    mainScreen: state.mainScreen,
+    user: state.auth,
+  };
+}
+
+MainScreen = connect(mapStateToProps, mapActionsToProps)(MainScreen);
+
+export default MainScreen;
