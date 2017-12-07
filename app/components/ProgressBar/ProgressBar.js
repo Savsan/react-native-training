f15d4d6cce777a8b84f757ecebcbdf738fd0b430
Aliaksandr Varabyou <aliaksandr_varabyou@epam.com>
2017-12-07 13:37:33 +0300

    TASK-4: add rise animation to progress scales

--- app/components/ProgressBar/ProgressBar.js
+++ app/components/ProgressBar/ProgressBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+import { View, Text, Animated, Easing } from 'react-native';
 
 import { colors } from 'typography';
 import styles from './styles';
@@ -8,22 +8,27 @@ import styles from './styles';
 export default class ProgressBar extends React.Component {
   constructor(props) {
     super(props);
+    this.scaleRiseValue = new Animated.Value(0);
     this.state = {
       progressBarWidth: 0,
       calculatedPercent: 0,
     };
   }
 
-  calculatePercent(width) {
-    const value = this.props.value > 100 ? 100 : this.props.value;
-    const progressBarWidth = !width ? this.state.progressBarWidth : width;
-    const calculatedPercent = (progressBarWidth / 100) * value;
+  componentDidMount() {
+    this.rise();
+  }
 
-
-    this.setState({
-      progressBarWidth,
-      calculatedPercent,
-    });
+  rise() {
+    Animated.timing(
+      this.scaleRiseValue,
+      {
+        toValue: 1,
+        duration: 700,
+        easing: Easing.easeOut,
+        delay: 100,
+      },
+    ).start();
   }
 
   handleScaleLayout = (evt) => {
@@ -31,11 +36,26 @@ export default class ProgressBar extends React.Component {
     this.calculatePercent(width);
   }
 
+  calculatePercent(width) {
+    const value = this.props.value > 100 ? 100 : this.props.value;
+    const progressBarWidth = !width ? this.state.progressBarWidth : width;
+    const calculatedPercent = (progressBarWidth / 100) * value;
+
+    this.setState({
+      progressBarWidth,
+      calculatedPercent,
+    });
+  }
+
   render() {
     const { title } = this.props;
     const scaleEndingStyle = this.props.value < 100
       ? styles.progressScaleBleed
       : null;
+    const rise = this.scaleRiseValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, this.state.calculatedPercent],
+    });
 
     return (
       <View style={styles.progressBarContainer}>
@@ -44,14 +64,14 @@ export default class ProgressBar extends React.Component {
           style={styles.progressBar}
           onLayout={this.handleScaleLayout}
         >
-          <View
+          <Animated.View
             style={[
               styles.progressScale,
-              { width: this.state.calculatedPercent },
+              { width: rise },
             ]}
           >
             <View style={scaleEndingStyle} />
-          </View>
+          </Animated.View>
         </View>
       </View>
     );
