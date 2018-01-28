c66f9b558194ff88c974eac45c3d1c8e20ffd181
Alexandr Varabyou <vorobev.profi@gmail.com>
2018-01-28 14:48:14 +0300

    TASK-5: Set detach options - Detach to android

---
+++ android/app/src/main/java/com/epam/app/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.epam.app.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "";
+
+}
