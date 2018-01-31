06e40007d3b63d6c4b5ee086b592dc7a516e81db
Alexandr Varabyou <vorobev.profi@gmail.com>
2018-01-31 17:38:26 +0300

    TASK-6: set configuration for react-native FCM module

--- android/app/src/main/java/com/epam/app/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/com/epam/app/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package com.epam.app.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp7803c07876f3461eb7e9d773a7dbf354://192.168.56.1:19000";
 
 }
