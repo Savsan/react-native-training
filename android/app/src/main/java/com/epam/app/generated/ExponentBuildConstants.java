c82d261044d2b56675c06ea60a7ecdaff1483d03
Alexandr Varabyou <vorobev.profi@gmail.com>
2018-02-17 22:38:13 +0300

    TASK-6-FCM added config

--- android/app/src/main/java/com/epam/app/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/com/epam/app/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package com.epam.app.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp7803c07876f3461eb7e9d773a7dbf354://192.168.56.1:19000";
+  public static final String DEVELOPMENT_URL = "exp7803c07876f3461eb7e9d773a7dbf354://t7-6wd.savsan.aliaksandr-varabyou.exp.direct:80";
 
 }
